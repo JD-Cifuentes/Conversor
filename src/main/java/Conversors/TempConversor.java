90444a3889740a8336dd84260256c3cb132db91a
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2023-03-12 20:08:56 -0500

    Temperature conversión completed so far

--- src/main/java/Conversors/TempConversor.java
+++ src/main/java/Conversors/TempConversor.java
@@ -1,52 +1,33 @@
 package Conversors;
 
+import com.sun.beans.editors.DoubleEditor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 public class TempConversor {
-    static Scanner sc = new Scanner(System.in); // Scanner Class
-
-    // Method to convert Celcius to Fahrenheit
-    static double C_F(double C){
+    public double CtoF(double C){
         double F = (C * 9/5) + 32;
         return F;
     }
-    // Method to convert Celcius to Kelvin
-    static double C_K(double C){
+    public double CtoK(double C){
         double K = C + 273.15;
         return K;
     }
-
-    // Method to convert Fahrenheit to Celcius
-    static double F_C(double F){
+    public double FtoC(double F){
         double C = (F - 32) * 5/9;
         return C;
     }
-    // Method to convert Fahrenheit to Kelvin
-    static double F_K(double F){
+    public double FtoK(double F){
         double K = (F - 32) * 5/9 + 273.15;
         return K;
     }
-
-    // Method to convert Kelvin to Celcius
-    static double K_C(double K){
+    public double KtoC(double K){
         double C = K - 273.15;
         return C;
     }
-    // Method to convert Kelvin to Fahrenheit
-    static double K_F(double K){
+    public double KtoF(double K){
         double F = (K - 273.15) * 9/5 + 32;
         return F;
     }
-
-    // Method to read the value of temperature given by the user
-    static double input(String word){
-        System.out.println("Enter "+word+" value:");
-        double val = sc.nextDouble();
-        return val;
-    }
-
-    // Method to print converted value of temperature
-    static void output(double val, String word){
-        System.out.printf("%s value: %.2f",word,val);
-    }
-
 }
--- src/main/java/Utilities/BttnConversionAction.java
+++ src/main/java/Utilities/BttnConversionAction.java
@@ -1,19 +1,25 @@
 package Utilities;
 
+import Conversors.TempConversor;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Method;
 
 public class BttnConversionAction implements ActionListener {
 
     private JTextField inValue;
     private JLabel outValue;
+    private JComboBox inComboBox;
+    private JComboBox outComboBox;
 
-    public BttnConversionAction(JTextField inValue, JLabel outValue) {
+    public BttnConversionAction(JTextField inValue, JLabel outValue, JComboBox inComboBox, JComboBox outComboBox) {
         this.inValue = inValue;
         this.outValue = outValue;
+        this.inComboBox = inComboBox;
+        this.outComboBox = outComboBox;
     }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -21,11 +27,27 @@ public class BttnConversionAction implements ActionListener {
             JOptionPane.showMessageDialog(null,"No has ingresado ningún valor.","Falta valor", JOptionPane.WARNING_MESSAGE);
         } else {
             try {
-                double entryvalue = Double.parseDouble(inValue.getText());
-                outValue.setText(String.valueOf(entryvalue*10));
+                ComboBoxKeyValue outBoxOption = (ComboBoxKeyValue) this.outComboBox.getSelectedItem();
+                ComboBoxKeyValue inBoxOption = (ComboBoxKeyValue) this.inComboBox.getSelectedItem();
+
+                if ( inBoxOption != outBoxOption){
+                    double entryValue = Double.parseDouble(inValue.getText());
+                    TempConversor convert = new TempConversor();
+                    Class<?> myClass = convert.getClass();
+                    String pair =inBoxOption.getSimbol()+"to"+outBoxOption.getSimbol();
+                    Method method = myClass.getMethod(pair, double.class);
+                    if(outBoxOption.getSimbol().equals("C") || outBoxOption.getSimbol().equals("F")){
+                        outValue.setText(String.format("%.2f", method.invoke(convert,entryValue)) + "°" + " " + outBoxOption.getSimbol());
+                    } else if (outBoxOption.getSimbol().equals("K")) {
+                        outValue.setText(String.format("%.2f", method.invoke(convert,entryValue)) + " " + outBoxOption.getSimbol());
+                    }
+
+                }else{
+                    JOptionPane.showMessageDialog(null,"Las unidades de entrada y salida son las mismas.","Unidades iguales",JOptionPane.WARNING_MESSAGE);
+                }
             }catch (Exception exception){
                 System.out.println(exception);
-                JOptionPane.showMessageDialog(null,"Valor ingresado no valido, solo se admiten números reales usando punto para el decimal","Valor invalido",JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(null,"Valor ingresado no valido, solo se admiten números reales usando coma para el decimal","Valor invalido",JOptionPane.WARNING_MESSAGE);
             }
         }
 
